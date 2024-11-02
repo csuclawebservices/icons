f4102c02ca5c5f5b71fb10732ec321441272a0a1
John Russell <john@johnrussell.dev>
2024-11-01 18:33:47 -0600

    Fixed a stroke issue with the testimonial icons

--- src/library/testimonial-testimony.js
+++ src/library/testimonial-testimony.js
@@ -1,6 +1,6 @@
 // -- This is an auto-generated file. Do not edit.
 
-import { SVG, Path, Rect } from '@wordpress/primitives';
+import { SVG, Path } from '@wordpress/primitives';
 
 const testimonialTestimony = (
     <SVG
@@ -9,14 +9,10 @@ const testimonialTestimony = (
         viewBox="0 0 24 24"
         xmlns="http://www.w3.org/2000/svg"
     >
-        <Rect
-            x="4.75"
-            y="4.75"
-            width="14.5"
-            height="14.5"
-            rx="1.25"
-            stroke="black"
-            strokeWidth="1.5"
+        <Path
+            fillRule="evenodd"
+            clipRule="evenodd"
+            d="M18 5.5H6C5.72386 5.5 5.5 5.72386 5.5 6V18C5.5 18.2761 5.72386 18.5 6 18.5H18C18.2761 18.5 18.5 18.2761 18.5 18V6C18.5 5.72386 18.2761 5.5 18 5.5ZM6 4C4.89543 4 4 4.89543 4 6V18C4 19.1046 4.89543 20 6 20H18C19.1046 20 20 19.1046 20 18V6C20 4.89543 19.1046 4 18 4H6Z"
         />
         <Path d="M13 8V12H16.0446V13.8394C16.0446 14.3066 15.9172 14.6569 15.7261 14.8321C15.3439 15.1825 14.707 15.0584 14.1338 15H13.9427V16H14.2611C14.8981 16 15.7261 15.9416 16.3631 15.4161C16.7452 15.0657 17 14.4818 17 13.781V8H13ZM7 12H10V13.8394C10 14.3066 9.87261 14.6569 9.68153 14.8321C9.29936 15.1825 8.66242 15.0584 8.08917 15H7.89809V16H8.21656C8.8535 16 9.68153 15.9416 10.3185 15.4161C10.7006 15.0657 10.9554 14.4818 10.9554 13.781V8H7L7 12Z" />
     </SVG>
