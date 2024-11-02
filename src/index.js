b29ca353c8889a6ce86752aecc4051da766107b0
John Russell <john@johnrussell.dev>
2024-11-01 18:28:29 -0600

    Added the "testimonial" and "testimonialTestimony" icons

--- src/index.js
+++ src/index.js
@@ -17,3 +17,5 @@ export { default as layout7 } from './library/layout-7';
 export { default as layout8 } from './library/layout-8';
 export { default as layout9 } from './library/layout-9';
 export { default as overflow } from './library/overflow';
+export { default as testimonialTestimony } from './library/testimonial-testimony';
+export { default as testimonial } from './library/testimonial';
---
+++ src/library/testimonial-testimony.js
@@ -0,0 +1,25 @@
+// -- This is an auto-generated file. Do not edit.
+
+import { SVG, Path, Rect } from '@wordpress/primitives';
+
+const testimonialTestimony = (
+    <SVG
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        xmlns="http://www.w3.org/2000/svg"
+    >
+        <Rect
+            x="4.75"
+            y="4.75"
+            width="14.5"
+            height="14.5"
+            rx="1.25"
+            stroke="black"
+            strokeWidth="1.5"
+        />
+        <Path d="M13 8V12H16.0446V13.8394C16.0446 14.3066 15.9172 14.6569 15.7261 14.8321C15.3439 15.1825 14.707 15.0584 14.1338 15H13.9427V16H14.2611C14.8981 16 15.7261 15.9416 16.3631 15.4161C16.7452 15.0657 17 14.4818 17 13.781V8H13ZM7 12H10V13.8394C10 14.3066 9.87261 14.6569 9.68153 14.8321C9.29936 15.1825 8.66242 15.0584 8.08917 15H7.89809V16H8.21656C8.8535 16 9.68153 15.9416 10.3185 15.4161C10.7006 15.0657 10.9554 14.4818 10.9554 13.781V8H7L7 12Z" />
+    </SVG>
+);
+
+export default testimonialTestimony;
